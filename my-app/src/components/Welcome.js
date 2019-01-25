7b064faf4612023f3e8319a98f51801e4a91dacc
Jr Cenina <44345078+jayAardvark@users.noreply.github.com>
2019-01-25 13:49:06 -0800

    created components

---
+++ my-app/src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        <h1>In or Out</h1>
+        <p>Helping you preserve your musical instrument!</p>
+      </div>
+    );
+  }
+}
