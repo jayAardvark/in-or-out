9a573e6c4eeabde868ae3dbf64936576a8d5d0fc
Jr Cenina <44345078+jayAardvark@users.noreply.github.com>
2019-01-25 17:36:25 -0800

    Form component and API call

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,40 @@ import React, { Component } from "react";
 import Welcome from "./components/Welcome";
 import Form from "./components/Form";
 import Output from "./components/Output";
+import API_KEY from "./config/keys";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  //retrieve data from API
+  retrieveData = async e => {
+    e.preventDefault();
+
+    let country = ""; //some zipcodes in US align to zipcodes in other countries
+    const location = e.target.zipcode.value;
+    if (e.target.country.value === "US") {
+      country = ",US";
+    }
+
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}${country}&appid=${API_KEY}&units=imperial`
+    );
+
+    //convert api_call data to json
+    const data = await api_call.json();
+
+    console.log(data);
+  };
+
   render() {
     return (
       <div>
         <Welcome />
-        <Form />
+        {/* The Form component will now have access to retrieveData function in props */}
+        <Form retrieveData={this.retrieveData} />
         <Output />
       </div>
     );
--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -3,9 +3,15 @@ import React, { Component } from "react";
 export default class Form extends Component {
   render() {
     return (
-      <div>
-        <p>Form Input</p>
-      </div>
+      <form onSubmit={this.props.retrieveData}>
+        {/* consider inputs for city and state for those inclined.  also, geolocation */}
+        <input type="text" name="zipcode" placeholder="Enter ZIP code" />
+        <select name="country">
+          <option value="US">US</option>
+          <option value="other">OTHER</option>
+        </select>{" "}
+        <button>Submit</button>
+      </form>
     );
   }
 }
